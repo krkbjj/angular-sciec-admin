f8635747002acd4799f6a453efc4564bb5964eac
Giovanni Carioca Ramos <krkbjj@gmail.com>
2018-05-06 22:01:47 -0300

    Correção app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,6 @@ import { RegisterComponent } from './register/register.component';
 import { HttpModule } from '@angular/http';
 import { routing} from './app.router';
 import {FormsModule} from '@angular/forms';
-import { AuthComponent } from './auth/auth.component';
-import { UserComponent } from './shared/user/user.component';
-import { AdminComponent } from './shared/admin/admin.component';
 
 
 @NgModule({
@@ -20,9 +17,6 @@ import { AdminComponent } from './shared/admin/admin.component';
     HomeComponent,
     LoginComponent,
     RegisterComponent,
-    AuthComponent,
-    UserComponent,
-    AdminComponent
   ],
   imports: [
     BrowserModule,
