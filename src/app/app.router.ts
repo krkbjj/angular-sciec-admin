e61f8f744b98ba69006094adae56531bd404fcf9
Gerverson <gerversonsa@gmail.com>
2018-05-18 15:06:01 -0300

    Rotas do usuário

--- src/app/app.router.ts
+++ src/app/app.router.ts
@@ -9,12 +9,16 @@ import { UserComponent } from './menu/user/user.component';
 import {OrgMeusEventosComponent} from './menu/org/org-meus-eventos/org-meus-eventos.component';
 import {OrgControleFrequenciaComponent} from './menu/org/org-controle-frequencia/org-controle-frequencia.component';
 import {OrgComponent} from './menu/org/org.component';
+import {ProfileComponent} from './menu/user/profile/profile.component';
+import {SubscriptionComponent} from './menu/user/subscription/subscription.component';
 
 
 const APP_ROUTES: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
   {path: 'user', component: UserComponent},
+  {path: 'user-profile', component: ProfileComponent},
+  {path: 'user-subscription', component: SubscriptionComponent},
   {path: 'admin', component: AdminComponent},
   {path: 'org-meus-eventos', component: OrgMeusEventosComponent},
   {path: 'org-controle-frequencia', component: OrgControleFrequenciaComponent},
